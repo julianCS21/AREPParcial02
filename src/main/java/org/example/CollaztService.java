7509c201a2fea9cabedc11a4292f8d7e2baf42f6
unknown <julian.castillo-s@labinfo.is.escuelaing.edu.co>
2023-10-24 14:48:00 -0500

    service finished

--- src/main/java/org/example/CollaztService.java
+++ src/main/java/org/example/CollaztService.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
+import java.util.ArrayList;
+
 import static spark.Spark.*;
 public class CollaztService {
 
@@ -7,7 +11,14 @@ public class CollaztService {
     public static void main(String... args){
         port(getPort());
         staticFiles.location("/public");
-        get("hello", (req,res) -> "Hello Docker!");
+        get("collazt", (req,res) -> {
+
+            res.type("text/json");
+
+            int number = Integer.parseInt(req.queryParams("number"));
+
+            return CollaztSecuence(number);
+        });
     }
 
     private static int getPort() {
@@ -15,6 +26,27 @@ public class CollaztService {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
+    }
+
+
+    public static String CollaztSecuence(int number){
+        ArrayList<String> secuence = new ArrayList<>();
+        secuence.add(String.valueOf(number));
+
+        while(number != 1 &&  number > 0){
+            if(number % 2 == 0){
+                number = number / 2;
+            }
+            else {
+                number = 3 * number + 1;
+            }
+            secuence.add(String.valueOf(number));
+        }
+        String result = secuence.get(0) + " ";
+        for(int i = 1; i < secuence.size(); i++){
+            result +=  "-> " + secuence.get(i);
+        }
+        return result;
     }
 
 
